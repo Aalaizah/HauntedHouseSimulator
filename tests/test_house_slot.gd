5187b8cc83999d95a8ed9216d838573363747aa7
Aalaizah <amm4108@gmail.com>
2024-12-17 10:13:37 -0600

    finished test_house_slot tests

--- tests/test_house_slot.gd
+++ tests/test_house_slot.gd
@@ -9,49 +9,94 @@ var testHouseSlot
 var roomSize = Vector2(Global.small_room_size, Global.small_room_size)
 var canDropData : bool
 var testRoom
+var testParentNode
 
 func before_each():
 	testHouseSlot = testHouseSlotScript.new()
 	testHouseSlot.init(testRoomSmall.room_size, roomSize)
+	testParentNode = testHouseSlotScript.new()
+	testParentNode.init(testRoomSmall.room_size, roomSize)
 	
-func test_drop_data_when_can_small():
+func test_can_drop_data_when_can_small():
 	testRoom = RoomItem.new()
 	testRoom.init(testRoomSmall)
 	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
 	assert_true(canDropData)
 
-func test_drop_data_when_cant_small():
-	pending('This test is not implemented yet')
+func test_can_drop_data_when_cant_small():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomSmall)
+	testHouseSlot.add_child(testRoom)
+	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
+	assert_false(canDropData)
 	
-func test_drop_data_when_can_med_long():
+func test_can_drop_data_when_can_med_long():
 	testRoom = RoomItem.new()
 	testRoom.init(testRoomMedLong)
 	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
 	assert_true(canDropData)
 
-func test_drop_data_when_cant_med_long():
-	pending('This test is not implemented yet')
+func test_can_drop_data_when_cant_med_long():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomMedLong)
+	testHouseSlot.add_child(testRoom)
+	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
+	assert_false(canDropData)
 	
-func test_drop_data_when_can_med_tall():
+func test_can_drop_data_when_can_med_tall():
 	testRoom = RoomItem.new()
 	testRoom.init(testRoomMedTall)
 	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
 	assert_true(canDropData)
 
-func test_drop_data_when_cant_med_tall():
-	pending('This test is not implemented yet')
+func test_can_drop_data_when_cant_med_tall():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomMedTall)
+	testHouseSlot.add_child(testRoom)
+	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
+	assert_false(canDropData)
 	
-func test_drop_data_when_can_large():
+func test_can_drop_data_when_can_large():
 	testRoom = RoomItem.new()
 	testRoom.init(testRoomLarge)
 	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
 	assert_true(canDropData)
 
-func test_drop_data_when_cant_large():
-	pending('This test is not implemented yet')
-	
-func test_can_drop_data():
-	pending('This test is not implemented yet')
+func test_cant_drop_data_when_cant_large():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomLarge)
+	testHouseSlot.add_child(testRoom)
+	canDropData = testHouseSlot._can_drop_data(roomSize, testRoom)
+	assert_false(canDropData)
 	
 func test_cant_drop_non_room_data():
-	pending('This test is not implemented yet')
+	canDropData = testHouseSlot._can_drop_data(roomSize, testRoomSmall)
+	assert_false(canDropData)
+	
+func test_drop_data_small():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomSmall)
+	testParentNode.add_child(testRoom)
+	testHouseSlot._drop_data(roomSize, testRoom)
+	assert_same(testRoom.get_parent(), testHouseSlot)
+	
+func test_drop_data_med_long():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomMedLong)
+	testParentNode.add_child(testRoom)
+	testHouseSlot._drop_data(roomSize, testRoom)
+	assert_same(testRoom.get_parent(), testHouseSlot)
+	
+func test_drop_data_med_tall():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomMedTall)
+	testParentNode.add_child(testRoom)
+	testHouseSlot._drop_data(roomSize, testRoom)
+	assert_same(testRoom.get_parent(), testHouseSlot)
+	
+func test_drop_data_large():
+	testRoom = RoomItem.new()
+	testRoom.init(testRoomLarge)
+	testParentNode.add_child(testRoom)
+	testHouseSlot._drop_data(roomSize, testRoom)
+	assert_same(testRoom.get_parent(), testHouseSlot)
